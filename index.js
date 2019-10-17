0c4c708005367ac980a0adc9919d1a746532b92a
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-10-17 19:42:30 -0400

    Added news generator.

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const apiKey = 'fbc548c48bb54a308f0e857ca9b041a3';
 
 function getNews() {
   fetch(
-    `https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-17&sortBy=publishedAt&apiKey=fbc548c48bb54a308f0e857ca9b041a3`
+    `https://newsapi.org/v2/everything?q=bitcoin&from=2019-09-17&sortBy=publishedAt&apiKey=${apiKey}`
   )
     .then(response => response.json())
-    .then(news => console.log(news));
+    .then(result => renderNews({ news: result.articles }));
 }
 
 getNews();
+
+function renderCard(title, imgLink, date) {
+  return `
+  <div class="card">
+    <img src=${imgLink} alt="" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">
+        <small class="text-muted">
+          ${data}
+        </small>
+      </p>
+    </div>
+  </div>
+  `;
+}
+
+function renderNews({ news }) {
+  const cols = document.getElementById('cardContainer');
+  news.map(
+    article =>
+      (cols.innerHTML += renderCard({
+        title: article.title,
+        imgLink: article.urlToImage,
+        date: article.publishDate,
+      }))
+  );
+}
